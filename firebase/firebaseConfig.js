3609ce55fd90ce0693f6716c8d5681527b5b3928
alphatg50 <guerkan.privat@gmail.com>
2024-11-05 07:41:35 +0100

    commit

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+     apiKey: "AIzaSyDUZ51ZSaWvYNfMgxRH3FmXIYDTk_sk2P0",
+    authDomain: "helpinformatik.firebaseapp.com",
+    projectId: "helpinformatik",
+    storageBucket: "helpinformatik.appspot.com",
+    messagingSenderId: "1036573792326",
+    appId: "1:1036573792326:web:763742e7cf12fc46ebd247",
+    measurementId: "G-K210BD89H6"
+  };
